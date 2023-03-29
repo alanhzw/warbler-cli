3f1557be79bdee3e886a6bb3435b3b842a33c0b0
warbler <1741847465@qq.com>
2023-03-29 15:38:45 +0800

    🚀 新增: 新增vitepress文档

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,5 +1,25 @@
 import { defineConfig } from 'vitepress';
+import sidebar from './sidebar/index';
 
 export default defineConfig({
   lang: 'en-US',
+  title: 'warbler-cli',
+  description: 'Command line interface for rapid JavaScript development',
+  lastUpdated: true,
+  appearance: 'dark',
+  themeConfig: {
+    siteTitle: 'warbler-cli',
+    nav: [],
+    docFooter: {
+      prev: '上一页',
+      next: '下一页',
+    },
+    lastUpdatedText: '最近更新',
+    socialLinks: [],
+    sidebar: sidebar.getSidebar(),
+    footer: {
+      message: 'Released under the MIT License.',
+      copyright: 'Copyright © 2023-present warbler',
+    },
+  },
 });
